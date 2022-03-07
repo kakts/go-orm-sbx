7c153f69c7b8d2d0f7e882d45a9002c92db31e02
kakts <kakts.git@gmail.com>
2022-03-08 01:14:07 +0900

    feat: use docker image for arm64

--- gorm_app/repository/config.go
+++ gorm_app/repository/config.go
@@ -19,8 +19,8 @@ func NewConfig() *Config {
 
 	// local環境設定
 	c.DB.Local.Host = "127.0.0.1"
-	c.DB.Local.Username = "root"
-	c.DB.Local.Password = "root"
+	c.DB.Local.Username = "docker"
+	c.DB.Local.Password = "docker"
 	c.DB.Local.DBName = "test_database"
 	c.DB.Local.Port = 3306
 	return c
